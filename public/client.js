5eaa7fdebf55338ba791c519cef5845a74835eb2
Erik Oja <erik_oja@yahoo.com>
2020-06-05 19:08:16 -0500

    filter issues by project

--- public/client.js
+++ public/client.js
@@ -17,14 +17,12 @@ function loadProjectList() {
       } 
       if (this.readyState == 4 && this.status == 200) {
         let res = JSON.parse(this.response)
-        //console.log(typeof res)
         let list = []
+        list.push(`<li class="projectName">All</li>`)
         res.forEach( x => {
           list.push(`<li class="projectName">${x}</li>`)
         })
         projectsList.innerHTML = list.join("")
-        //projects = document.querySelectorAll(".projectName")
-        //console.log(projects)
         resolve()
       }
     }
@@ -45,32 +43,8 @@ function loadIssues() {
       } 
       if (this.readyState == 4 && this.status == 200) {
         let res = JSON.parse(this.response)
-        //console.log(res)
-        let allIssues = []
-        res.forEach( x => {
-          let issues = []
-          x.issues.forEach( y => {
-            let status = y.open ? "Open" : "Closed"
-            let statusClass = y.open ? "" : "closed"
-            //console.log("status " + status + " " + y.open)
-            issues.push(`<div class="issue ${statusClass}">
-                          <h3>${x.project}</h3>
-                          <h5>Issue:</h5>
-                          <p>${y.issue}</p>
-                          <h5>Created by:</h5>
-                          <p>${y.createdBy}</p>
-                          <h5>Assigned To:</h5>
-                          <p>${y.assignedTo}</p>
-                          <h5>Date Created:</h5>
-                          <p>${y.date}</p>
-                          <h5>Last Updated</h5>
-                          <p>${y.lastUpdated}</p>
-                          <h5>${status}</h5>
-                        </div>`)
-          })
-          allIssues.push(issues.join(""))
-        })
-        issues.innerHTML = allIssues.join("")
+        let allIssues = createIssuesHTML(res)
+        issues.innerHTML = allIssues
         resolve()
       }
     }
@@ -79,48 +53,97 @@ function loadIssues() {
   })  
 }
 
+// page should now be loaded and can query DOM elements
 function pageLoaded() {
+   // filter issues by project name
   const projects = document.querySelectorAll(".projectName")
-  //console.log(projects)
   projects.forEach( x => {
-    x.addEventListener("click", filterByProjectName)
+    //console.log(x.innerHTML)
+    // display all project's issues
+    if (x.innerHTML == "All") x.addEventListener("click", loadIssues)
+    // display issues by project
+    else x.addEventListener("click", filterByProjectName)
   })
 
-  function filterByProjectName(e) {
-    console.log(e.target.innerHTML)
-    let xhttp = new XMLHttpRequest()
-    xhttp.onreadystatechange = function() {
-      console.log(this.readyState + ' ' + this.status)
-      if (this.readyState == 4 && this.status == 200) {
-        let res = JSON.parse(this.response)
-        const issues = document.getElementById("issues")
-        while (issues.firstElementChild) {
-          issues.firstElementChild.remove()
-        }
-        let project = res.project
-        let projectIssues = []
-        res.issues.forEach( x => {
-          let status = y.open ? "Open" : "Closed"
-          let statusClass = y.open ? "" : "closed"
-          issues.push(`<div class="issue ${statusClass}">
-                        <h3>${project}</h3>
-                        <h5>Issue:</h5>
-                        <p>${x.issue}</p>
-                        <h5>Created by:</h5>
-                        <p>${x.createdBy}</p>
-                        <h5>Assigned To:</h5>
-                        <p>${x.assignedTo}</p>
-                        <h5>Date Created:</h5>
-                        <p>${x.date}</p>
-                        <h5>Last Updated</h5>
-                        <p>${x.lastUpdated}</p>
-                        <h5>${status}</h5>
-                      </div>`)
-        })
-        issues.innerHTML = projectIssues.join("")
-      }
+  
+}
+
+// filters project issues by project
+function filterByProjectName(e) {
+  const issues = document.getElementById("issues")
+  let xhttp = new XMLHttpRequest()
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status >= 400) {
+      console.log("error loading issues")
+    } 
+    if (this.readyState == 4 && this.status == 200) {
+      let res = JSON.parse(this.response)
+      let allIssues = createIssuesHTML(res)
+      issues.innerHTML = allIssues
     }
-    xhttp.open("GET", `/projects/${e.target.innerHTML}`, false)
-    xhttp.send()
+  }
+  xhttp.open("GET", `/projects/${e.target.innerHTML}`, true)
+  xhttp.send()
+}
+
+// creates the HTML for the issues
+function createIssuesHTML(res) {
+  let allIssues = []
+  res.forEach( x => {
+    let issues = []
+    x.issues.forEach( y => {
+      let status = y.open ? "Open" : "Closed"
+      let statusClass = y.open ? "" : "closed"
+      issues.push(`<div class="issue ${statusClass}">
+                    <h3>${x.project}</h3>
+                    <h5>Issue:</h5>
+                    <p>${y.issue}</p>
+                    <h5>Created by:</h5>
+                    <p>${y.createdBy}</p>
+                    <h5>Assigned To:</h5>
+                    <p>${y.assignedTo}</p>
+                    <h5>Date Created:</h5>
+                    <p>${y.date}</p>
+                    <h5>Last Updated</h5>
+                    <p>${y.lastUpdated}</p>
+                    <h5>${status}</h5>
+                  </div>`)
+    })
+    allIssues.push(issues.join(""))
+  })
+  return allIssues.join("")
+}
+
+// sort issues
+const sort = document.getElementById("sort")
+sort.addEventListener("change", sortIssues)
+
+function sortIssues(e) {
+  console.log(e.target.value)
+  if (e.target.value == "newest") {
+    const issues = document.getElementById("issues")
+    const issuesToSort = Array.from(document.querySelectorAll(".issue"))
+    //let elements = Array.from(issues)
+    console.log(issues)
+    let dates = []
+    let sortedIssues = []
+    issuesToSort.forEach( x => {
+      //console.log(x.childNodes[17].innerHTML)
+      dates.push(x.childNodes[17].innerHTML)
+    })
+    dates.sort( (a, b) => b - a)
+    console.log(dates)
+    dates = Array.from(new Set(dates))
+    console.log(dates)
+    for (let i = 0; i < dates.length; i++) {
+      issuesToSort.forEach( x => {
+        if (x.childNodes[17].innerHTML == dates[i]) {
+          let html = createIssuesHTML(x)
+          sortIssues.push(Array.from(html))
+        }
+      })
+    }
+    console.log(sortedIssues)
+    issues.innerHTML = sortedIssues
   }
 }
--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,20 @@ module.exports = function(app) {
       res.send(projectsList)
     })
 
+   app.route("/projects/:project")
+    .get(function (req, res) {
+      //console.log("param: " + req.params.project)
+      let projectIssues = []
+      projects.forEach( x => {
+        if (req.params.project == x.project) {
+          //console.log("pushing project")
+          projectIssues.push(x)
+        }
+      })
+      //console.log(projectIssues)
+      res.send(projectIssues)
+    })
+
   app.route("/issues")
   .get(function (req, res) {
     res.send(projects)
