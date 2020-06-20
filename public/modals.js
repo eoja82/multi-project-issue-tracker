b1807ad2b4d1530eb795089fc03ee04380253463
Erik Oja <erik_oja@yahoo.com>
2020-06-19 19:55:18 -0500

    modals for login, adduser, basic style

--- public/modals.js
+++ public/modals.js
@@ -2,18 +2,24 @@
 
 const newIssueModal = document.getElementById("newIssueModal")
 const createNewIssue = document.getElementById("createNewIssue")
+const loginModal = document.getElementById("loginModal")
+const login = document.getElementById("login")
+const addUserModal = document.getElementById("addUserModal")
+const addUser = document.getElementById("addUser")
 const close = document.querySelectorAll(".close")
 const modifyModal = document.getElementById("modifyModal")
 const closeIssue = document.getElementById("closeIssue")
 
 createNewIssue.addEventListener("click", displayModal)
+login.addEventListener("click", displayModal)
+addUser.addEventListener("click", displayModal)
 addEventListener("click", closeModal)
 close.forEach( x => x.addEventListener("click", closeModal))
 // the event listener for the modify modal is in the page loaded function in client.js because it needs to be added after the data is loaded
 
 
 function displayModal(e) {
-  //console.log(e.target.className)
+  //console.log(e.target.id)
   if (e.target.id == "createNewIssue") newIssueModal.style.display = "block"
   if (e.target.className == "updateDelete") {
     modifyModal.style.display = "block"
@@ -51,11 +57,20 @@ function displayModal(e) {
     deleteInput[0].value = project
     deleteInput[1].value = id
   }
+  if (e.target.id == "login") loginModal.style.display = "block"
+  if (e.target.id == "addUser") addUserModal.style.display = "block"
 }
 
 function closeModal(e) {
-  if (e.target.id == "newIssueModal" || e.target.id == "modifyModal" || e.target.className == "close") {
+  // could I use `` here to shorten this up
+  if (e.target.id == "newIssueModal" || 
+      e.target.id == "modifyModal" ||  
+      e.target.id == "loginModal" ||
+      e.target.id == "addUserModal" ||
+      e.target.className == "close") {
     newIssueModal.style.display = "none"
     modifyModal.style.display = "none"
+    loginModal.style.display = "none"
+    addUserModal.style.display = "none"
   }
 }
