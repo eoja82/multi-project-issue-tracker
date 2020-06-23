417a89497d15364f3df28cdbeec6ca4666ac05d1
Erik Oja <erik_oja@yahoo.com>
2020-06-22 19:15:53 -0500

    add express-session

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const session = require("express-session")
+const MongoStore = require('connect-mongo')(session)
 const apiRoutes = require("./routes/api.js")
 const auth = require("./routes/auth.js")
 
@@ -17,12 +19,20 @@ mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopolo
   }
 )
 
-/* app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html')
-  }) */
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    collection: "sessions"  
+  }),
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+}
+}))
 
-//Routing for API 
+//Routing for API and authentication
 apiRoutes(app) 
 auth(app)
 
