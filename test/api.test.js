6596f7f9c7e80b1060d3681ad36eb57d3f0ad660
Erik Oja <erik_oja@yahoo.com>
2021-01-01 17:57:24 -0600

    GET and POST tests

--- test/api.test.js
+++ test/api.test.js
@@ -27,29 +27,33 @@ const projectData = [
 ]
       
 if (process.env.TEST) {
-  describe("routes", function() {
+  let requester
 
-    before(function(done) {
-      projectData.forEach( data => {
-        project = new Project(data)
-        project.save(function(err, data) {
-          if (err) console.log("Error saving project",err)
-        })
+  before(function(done) {
+    projectData.forEach( data => {
+      project = new Project(data)
+      project.save(function(err, data) {
+        if (err) console.log("Error saving project",err)
       })
-      done()
     })
+    requester = chai.request(app).keepOpen()
+    done()
+  })
 
-    /* after(function(done) {
-      Project.deleteMany({}, function(err) {
-        if (err) console.log("Error deleting projects")
-      })
-      done()
+  after(function(done) {
+    requester.close()
+    /* Project.deleteMany({}, function(err) {
+      if (err) console.log("Error deleting projects")
     }) */
-
-    describe("GET routes", function() {
+    done()
+  })
+  
+  describe("routes", function() {
+    describe("GET routes", function() { 
       describe("GET '/pageData'", function() {
         it("should send page data", function(done) {
-          chai.request(app)
+          //chai.request(app)
+          requester
             .get("/pageData")
             .end(function(err, res) {
               expect(err).to.not.be.an("error")
@@ -59,6 +63,77 @@ if (process.env.TEST) {
             })
         })
       })
+      describe("GET '/projects/:project'", function() {
+        it("should get one project", function(done) {
+          requester
+            .get(`/projects/${projectData[0].project}`)
+            .end(function(err, res) {
+              expect(err).to.not.be.an("error")
+              expect(res.status).to.equal(200)
+              expect(res.body.length).to.equal(1)
+              done()
+            })
+        })
+      })
+    })
+
+    describe("POST routes", function() {
+      describe("POST /create-or-modify-issue", function() {
+        it("should create a new issue for existing project", function(done) {
+          requester
+            .post("/create-or-modify-issue")
+            .type("form")
+            .send({
+              project: projectData[0].project,
+              issue: "A second issue.",
+              createdBy: "User 1"
+            })
+            .end(function(err, res) {
+              expect(err).to.not.be.an("error")
+              expect(res.status).to.equal(200)
+              expect(res.text).to.equal(`New issue for ${projectData[0].project} was created!`)
+              done()
+            })
+        })
+        it("should create new project and issue", function(done) {
+          requester
+            .post("/create-or-modify-issue")
+            .type("form")
+            .send({
+              project: "New Project",
+              issue: "New issue.",
+              createdBy: "User 1"
+            })
+            .end(function(err, res) {
+              expect(err).to.not.be.an("error")
+              expect(res.status).to.equal(200)
+              expect(res.text).to.equal(`New project and issue successfully created for New Project!`)
+              done()
+            })
+        })
+      })
+      describe("POST /issues/filter", function() {
+        it("should filter issues", function(done) {
+          let issueCount = 0
+          requester
+            .post("/issues/filter")
+            .type("form")
+            .send({
+              project: "All",
+              createdBy: "User 2",
+              assignedTo: "All",
+              status: "All"
+            })
+            .end(function(err, res) {
+              expect(err).to.not.be.an("error")
+              expect(res.status).to.equal(200)
+              res.body.forEach( x => issueCount += x.issues.length)
+              expect(issueCount).to.equal(1)
+              done()
+            })
+        })
+      })
     })
   })
+  
 }
