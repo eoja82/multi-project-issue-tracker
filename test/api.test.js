12934a846178979b31851aaaf6ffc4b1848c7dec
Erik Oja <erik_oja@yahoo.com>
2020-12-31 19:13:06 -0600

    run tests only if set to test in env

--- test/api.test.js
+++ test/api.test.js
@@ -4,6 +4,8 @@ const chai = require("chai"),
       app = require("../server.js"),
       { Project } = require("../routes/api.js"),
       { Users } = require("../routes/auth.js")
+      
+require('dotenv').config()
 
 chai.use(chaiHttp)
 
@@ -24,37 +26,39 @@ const projectData = [
   }
 ]
       
-describe("routes", function() {
+if (process.env.TEST) {
+  describe("routes", function() {
 
-  before(function(done) {
-    projectData.forEach( data => {
-      project = new Project(data)
-      project.save(function(err, data) {
-        if (err) console.log("Error saving project",err)
+    before(function(done) {
+      projectData.forEach( data => {
+        project = new Project(data)
+        project.save(function(err, data) {
+          if (err) console.log("Error saving project",err)
+        })
+      })
+      done()
+    })
+
+    /* after(function(done) {
+      Project.deleteMany({}, function(err) {
+        if (err) console.log("Error deleting projects")
+      })
+      done()
+    }) */
+
+    describe("GET routes", function() {
+      describe("GET '/pageData'", function() {
+        it("should send page data", function(done) {
+          chai.request(app)
+            .get("/pageData")
+            .end(function(err, res) {
+              expect(err).to.not.be.an("error")
+              expect(res.status).to.equal(200)
+              expect(res.body.pageData.length).to.equal(2)
+              done()
+            })
+        })
       })
     })
-    done()
   })
-
-  after(function(done) {
-    Project.deleteMany({}, function(err) {
-      if (err) console.log("Error deleting projects")
-    })
-    done()
-  })
-
-  describe("GET routes", function() {
-    describe("GET '/pageData'", function() {
-      it("should send page data", function(done) {
-        chai.request(app)
-          .get("/pageData")
-          .end(function(err, res) {
-            expect(err).to.not.be.an("error")
-            expect(res.status).to.equal(200)
-            expect(res.body.pageData.length).to.equal(2)
-            done()
-          })
-      })
-    })
-  })
-})
+}
