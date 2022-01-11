28c8288bb486eb4415677c50c66a78f6de33a8f2
Erik Oja <erik_oja@yahoo.com>
2022-01-11 17:40:10 -0600

    delete update OR delete js

--- public/modals.js
+++ public/modals.js
@@ -14,39 +14,6 @@ close.forEach( x => x.addEventListener("click", closeModal))
 
 function displayModal(e) {
   if (e.target.id == "newIssue") newIssueModal.style.display = "block"
-  if (e.target.className == "updateDelete") {
-    modifyModal.style.display = "block"
-
-    // populate the form input values with data from the issue on which the button was clicked
-    // the below indexes will need to be changed if issue data displayed is changed
-    const nodes = e.target.parentElement.childNodes
-    let project = nodes[1].textContent
-    let issue = nodes[5].textContent
-    let createdBy = nodes[9].textContent
-    let assignedTo = nodes[13].textContent
-    let status = nodes[25].textContent
-    let closeIssueChecked 
-    switch(status) {
-      case "Closed": closeIssueChecked = true
-        break
-      case "Open": closeIssueChecked = false
-        break
-      default: closeIssueChecked = false
-    }
-    let id = nodes[29].textContent
-
-    const modifyInput = document.querySelectorAll(".modifyInput")
-    modifyInput[0].value = project
-    modifyInput[1].value = issue
-    modifyInput[2].value = createdBy
-    modifyInput[3].value = assignedTo
-    modifyInput[4].value = id
-    closeIssue.checked = closeIssueChecked
-
-    const deleteInput = document.querySelectorAll(".deleteInput")
-    deleteInput[0].value = project
-    deleteInput[1].value = id
-  }
   if (e.target.id == "login") loginModal.style.display = "block"
 }
 
